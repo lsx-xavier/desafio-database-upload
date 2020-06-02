c716e0bd97ad9ecfc1879d0087b0e3fd57c012f7
Lucas Xavier <ls.xxavier@gmail.com>
2020-06-02 00:03:51 -0300

    Challange 6: Not good, need more pratice about this objects

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transactions from '../models/Transaction';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
+  public async execute(id: string): Promise<void> {
     // TODO
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transactionId = await transactionsRepository.findOne(id);
+
+    if (!transactionId) {
+      throw new AppError('The transaction does not exist!');
+    }
+
+    await transactionsRepository.remove(transactionId);
   }
 }
 
